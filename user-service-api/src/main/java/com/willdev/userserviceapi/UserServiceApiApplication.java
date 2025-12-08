db67911d09ef7703ca5c01c289c760a12a1007d5
wilson evangelista dos santos filho <wilson.evangelista22@gmail.com>
2025-12-08 13:12:21 -0300

    :sparkles:feature: add user repository

--- user-service-api/src/main/java/com/willdev/userserviceapi/UserServiceApiApplication.java
+++ user-service-api/src/main/java/com/willdev/userserviceapi/UserServiceApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServiceApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApiApplication.class, args);
+    }
 
 }
