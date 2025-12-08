b2857e84ac5cac774c9f8a8a3bbf0123f29433f9
wilson evangelista dos santos filho <wilson.evangelista22@gmail.com>
2025-12-08 00:17:05 -0300

    :sparkles:feature: iniciando e configurando projeto user service api

---
+++ user-service-api/src/test/java/com/willdev/userserviceapi/UserServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willdev.userserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
