26bda99190d238ba3de7f2da830606de8743a8b2
wilson evangelista dos santos filho <wilson.evangelista22@gmail.com>
2025-12-08 11:09:11 -0300

    :wrench:chore: add lib commons

---
+++ hd-commons-lib/src/main/java/com/willdev/hdcommonslib/models/enums/ProfileEnum.java
@@ -0,0 +1,21 @@
+package com.willdev.hdcommonslib.models.enums;
+
+import java.util.Arrays;
+
+public enum ProfileEnum {
+    ROLE_ADMIN("ROLE_ADMIN"), ROLE_CUSTOMER("ROLE_CUSTOMER"), ROLE_TECHNICIAN("ROLE_TECHNICIAN");
+
+    private final String description;
+
+    ProfileEnum(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static ProfileEnum toEnum(String description) {
+        return Arrays.stream(ProfileEnum.values()).filter(profileEnum -> profileEnum.description.equals(description)).findFirst().orElseThrow(() -> new IllegalArgumentException("Invalid description" + description));
+    }
+}
